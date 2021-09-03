fb3b61a5a5634d04d25a658f933e007a5b8bc731
gustavo3x2 <gustavo3x2@gmail.com>
2021-09-02 21:14:54 -0300

    Progeto concluido

---
+++ src/Content.js
@@ -0,0 +1,25 @@
+function Content() {
+    return(
+        <div className="conteudo">
+          <div className="center">
+            <div className="conteudo-single">
+              <h3>Titulo da camada</h3>
+              <p>Aqui e meu contéudo de exemplo o site da danki code no youtube! Espere que vc goste :)</p>
+            </div>
+            <div className="conteudo-single">
+              <h3>Titulo da camada</h3>
+              <p>Aqui e meu contéudo de exemplo o site da danki code no youtube! Espere que vc goste :)</p>
+            </div>
+            <div className="conteudo-single">
+              <h3>Titulo da camada</h3>
+              <p>Aqui e meu contéudo de exemplo o site da danki code no youtube! Espere que vc goste :)</p>
+            </div>
+            <div className="conteudo-single">
+              <h3>Titulo da camada</h3>
+              <p>Aqui e meu contéudo de exemplo o site da danki code no youtube! Espere que vc goste :)</p>
+            </div>
+          </div>
+        </div>
+    )
+}
+export default Content;
